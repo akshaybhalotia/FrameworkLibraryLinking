fe3de347a173b7e70b288109a498e42f78443641
akshaybhalotia <akshay.bhalotia@razorpay.com>
2016-09-08 12:07:14 +0530

    Add framework

---
+++ Framework/TestFramework/TestFramework/Test.m
@@ -0,0 +1,21 @@
+//
+//  Test.m
+//  TestFramework
+//
+//  Created by Akshay Bhalotia on 08/09/16.
+//  Copyright © 2016 Akshay Bhalotia. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
++ (BOOL)working {
+  return YES;
+}
+
+- (BOOL)objectsWorkingToo {
+  return YES;
+}
+
+@end
