114cce9bf858e7d8b281be8a494cf515e45f7d02
akshaybhalotia <akshay.bhalotia@razorpay.com>
2016-09-16 12:34:15 +0530

    Works now with some tweaks

---
+++ Project/TestApp/TestApp/TestFramework.framework/Versions/A/Headers/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  TestFramework
+//
+//  Created by Akshay Bhalotia on 08/09/16.
+//  Copyright © 2016 Akshay Bhalotia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
++ (BOOL)working;
+- (BOOL)objectsWorkingToo;
+
+@end
